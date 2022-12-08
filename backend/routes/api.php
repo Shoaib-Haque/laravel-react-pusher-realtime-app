be12e8fd28ea412525fcedf58557bf1d6f5adb73
shaoib <shoaibhaque7@gmail.com>
2022-12-08 13:21:10 +0600

    added api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+// Auth
+use App\Http\Controllers\Auth\AdminAuthController;
+use App\Http\Controllers\Auth\UserAuthController;
+// Users
+use App\Http\Controllers\User\UserController;
+// Messages
+use App\Http\Controllers\User\MessageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +24,32 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'admin'],
+    Route::group(['prefix' => 'admin'],
+        function ($router) {
+            Route::post('/login', [AdminAuthController::class, 'login']);
+            Route::post('/logout', [AdminAuthController::class, 'logout']);
+        }
+    ),
+);
+
+Route::group(['middleware' => 'user'],
+    Route::group(['prefix' => 'user'],
+        function ($router) {
+            Route::post('/login', [UserAuthController::class, 'login']);
+            Route::post('/logout', [UserAuthController::class, 'logout']);
+        }
+    ),
+    Route::group(['prefix' => 'users'],
+        function ($router) {
+            Route::get('/', [UserController::class, 'index']);
+        }
+    ),
+    Route::group(['prefix' => 'messages'],
+        function ($router) {
+            Route::get('/{receiver_id}', [MessageController::class, 'index']);
+            Route::post('/', [MessageController::class, 'send']);
+        },
+    ),
+);
