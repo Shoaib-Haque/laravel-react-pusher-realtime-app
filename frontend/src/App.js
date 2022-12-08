cbf28b508a92cadb92686c8fdb7b4c3b6ddbc4bd
XI-SHB <khchow@xandi.co.jp>
2022-12-08 12:53:47 +0600

    added code on app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+import Login from "./components/Auth/Index.component";
+import Dashboard from "./components/Dashboard/Index.component";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container fluid className="p-0">
+        <Row>
+          <Col>
+            <Routes>
+              <Route path="/" element={<Navigate replace to="/login" />} />
+              {/* Auth */}
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute>
+                    <Login />
+                  </PublicRoute>
+                }
+              />
+              {/* Dashboard */}
+              <Route
+                path="/dashboard"
+                element={
+                  <UserProtectedRoute>
+                    <Dashboard />
+                  </UserProtectedRoute>
+                }
+              />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </Router>
   );
+}
+function PublicRoute({ children }) {
+  let isAuthenticated = getAuth();
+  if (isAuthenticated) {
+    if (getRole() === "admin") return <Navigate to="/accounts" />;
+    else if (getRole() === "account") return <Navigate to="/statements" />;
+    else if (getRole() === "user") return <Navigate to="/dashboard" />;
+  } else {
+    return children;
+  }
+}
+
+function AdminProtectedRoute({ children }) {
+  let isAuthenticated = getAuth();
+  return isAuthenticated ? (
+    getRole() === "admin" ? (
+      children
+    ) : (
+      <Navigate to="/statements" />
+    )
+  ) : (
+    <Navigate to="/login" />
+  );
+}
+
+function UserProtectedRoute({ children }) {
+  let isAuthenticated = getAuth();
+  return isAuthenticated ? (
+    getRole() === "user" ? (
+      children
+    ) : (
+      <Navigate to="/dashboard" />
+    )
+  ) : (
+    <Navigate to="/login" />
+  );
+}
+
+function getAuth() {
+  let token = localStorage.getItem("authToken");
+  return token;
+}
+
+function getRole() {
+  let role = localStorage.getItem("role");
+  return role;
 }
 
 export default App;
