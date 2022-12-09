80839baa6b50f23325245bcf3a15eb136c7d6827
shaoib <shoaibhaque7@gmail.com>
2022-12-09 10:18:25 +0600

    implements ShouldBroadcast with event

--- backend/app/Events/MessageEvent.php
+++ backend/app/Events/MessageEvent.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use App\Models\Messages;
 
-class MessageEvent
+class MessageEvent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     private $id, $sender_id, $receiver_id, $message, $created_at, $update_at;
